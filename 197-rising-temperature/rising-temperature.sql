dea847a1aa851d7c3d1f664794103b7b60e92ee2
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-02-23 21:16:41 +0530

    Time: 1394 ms (10.51%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 select w1.id 
 from Weather w1  
-where w1.temperature >(select temperature from Weather w2 where datediff(w1.recordDate,w2.recordDate)=1 order by recordDate) order by recordDate;
+where w1.temperature >(select temperature from Weather w2 where datediff(w1.recordDate,w2.recordDate)=1) order by recordDate;
