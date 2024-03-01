c60bbfe88b2ba0a251164220cafd8244caea17e9
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-01 23:03:22 +0530

    Time: 228 ms (18.99%) | Memory: 102.3 MB (93.83%) - LeetSync

---
+++ 2483-task-scheduler-ii/task-scheduler-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+ 
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        map<int,long long>mp;
+        long long days=0;
+        for(int i=0;i<tasks.size();i++)
+        {
+            if(mp.find(tasks[i])==mp.end())
+            {
+                days++;
+                mp[tasks[i]]=days;
+            }
+            else
+            {
+                days++;
+                long long a=days-(mp[tasks[i]]+1);
+                if(a<space)
+                {
+                    days=days+(space-a);
+                }
+                mp[tasks[i]]=days;
+            }
+        }
+        return days;
+    }
+};
