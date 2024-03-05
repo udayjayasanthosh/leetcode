5bf820583db85cf8139979bebd05730dfbde7025
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-05 18:08:12 +0530

    Time: 24 ms (65.52%) | Memory: 13.8 MB (70.34%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,28 +3,29 @@ public:
     int minimumLength(string s) {
         int i=0;
         int j=s.size()-1;
-        while(i<j && s[i] == s[j])
+        while(i<=j)
         {
-            // if(i==j)return 1;
-            // int a=s[i];
-            // if(a==s[j])
-            // {
-            while(s[i+1]==s[i] and i<j)
+            if(i==j)return 1;
+            int a=s[i];
+            if(a==s[j])
+            {
+            while(a==s[i] and i<=j)
             {
                 i++;
                 //cout<<i<<"__"<<endl;
             }
-            while(s[j-1]==s[j] and i<j)
+            while(a==s[j] and i<=j)
             {
                 j--;
                 //cout<<j<<"--"<<endl;
             }
-            i++;
-            j--;
-            // else
-            // {
-            //     break;
-            // }
+            }
+            // i++;
+            // j--;
+            else
+            {
+                break;
+            }
         }
         // if(s[i]==s[j])return 0;
         // else
