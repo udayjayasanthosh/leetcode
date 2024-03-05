d1b23efe3448e7092784b11d0107b400af2a1521
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-05 18:06:22 +0530

    Time: 27 ms (45.86%) | Memory: 13.9 MB (44.14%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,27 +3,28 @@ public:
     int minimumLength(string s) {
         int i=0;
         int j=s.size()-1;
-        while(i<=j)
+        while(i<j && s[i] == s[j])
         {
-            if(i==j)return 1;
-            int a=s[i];
-            if(a==s[j])
-            {
-            while(a==s[i] and i<=j)
+            // if(i==j)return 1;
+            // int a=s[i];
+            // if(a==s[j])
+            // {
+            while(s[i+1]==s[i] and i<j)
             {
                 i++;
-                cout<<i<<"__"<<endl;
+                //cout<<i<<"__"<<endl;
             }
-            while(a==s[j] and i<=j)
+            while(s[j-1]==s[j] and i<j)
             {
                 j--;
-                cout<<j<<"--"<<endl;
+                //cout<<j<<"--"<<endl;
             }
-            }
-            else
-            {
-                break;
-            }
+            i++;
+            j--;
+            // else
+            // {
+            //     break;
+            // }
         }
         // if(s[i]==s[j])return 0;
         // else
