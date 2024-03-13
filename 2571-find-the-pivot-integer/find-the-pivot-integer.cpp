9550ea8253327e9527897282cdc1b226a30ee2f7
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-13 07:50:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -4,11 +4,11 @@ public:
         if(n==1)return 1;
         int mid=n/2;
         int sum=(n*(n+1))/2;
-        cout<<sum<<"++"<<endl;
+        // cout<<sum<<"++"<<endl;
         for(int i=mid;i<n;i++)
         {
             int a=(i*(i+1))/2;
-            cout<<a<<"--"<<endl;
+            // cout<<a<<"--"<<endl;
             if(a==sum+i-a)return i;
         }
         return -1;
