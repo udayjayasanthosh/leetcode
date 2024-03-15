67e08a9ad6b38ad3d184ad528a57afc8bff18203
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-15 21:56:01 +0530

    Time: 62 ms (22.85%) | Memory: 37.9 MB (40.92%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int i=0;
+        int j=0;
+        int sum=0;
+        int c=0;
+        int c1=count(nums.begin(),nums.end(),0);
+        
+        
+        if (c1==nums.size() and goal==0)return (c1*(c1+1))/2;
+        map<int,int>mpp;
+        
+        for (int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if (sum==goal)c+=1;
+            if ( mpp.find(sum-goal)!=mpp.end()){
+               
+                c+=mpp[sum-goal];//111
+            }
+            mpp[sum]++;//1:1
+            
+        }
+         return c;
+        
+    }
+};
