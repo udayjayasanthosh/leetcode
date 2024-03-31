20747981630736c249e90a20d7615bce720b72bc
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-03-31 08:36:25 +0530

    Time: 196 ms (5.05%) | Memory: 82.6 MB (20.21%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.cpp
@@ -5,14 +5,15 @@ public:
         int maxi=-1, mini=-1;
         int s=nums.size();
         for(int r=0, l=0; r<s; r++){
-            int&& x=move(nums[r]);
+            int x=nums[r];
             if (x<minK ||x>maxK){// x exceeds the bound
                 l=r+1; // move l to r+1
-                continue;
+                // continue;
             }
             if (x==maxK) maxi=r; // position for maxK
             if (x==minK) mini=r; // position for minK
             ans+=max((min(maxi, mini)-l+1),0);
+            cout<<ans;
         }
         return ans;
     }
