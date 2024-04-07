dc1f72731d5ad7c3929a2bd3512abb2e6d5c342f
udayjayasanthosh <144417335+udayjayasanthosh@users.noreply.github.com>
2024-04-07 10:32:18 +0530

    Time: 3 ms (45.50%) | Memory: 7.2 MB (75.32%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int open=0;
+        int star=0;
+        int close=0;
+        for(auto i:s)
+        {
+            if(i=='(')
+            {
+                open++;
+            }
+            else if(i==')')
+            {
+                // close++;
+                if(open>0)
+                {
+                    open--;
+                }
+                else if(star>0)
+                {
+                    star--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                star++;
+            }
+        }
+        if(open>0)
+        {
+            open=0;
+            star=0;
+            close=0;
+            for(int i=s.size()-1;i>=0;i--)
+            {
+                if(s[i]==')')
+                {
+                    close++;
+                }
+                else if(s[i]=='(')
+                {
+                    if(close>0)
+                    {
+                        close--;
+                    }
+                    else if(star>0)
+                    {
+                        star--;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    star++;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
